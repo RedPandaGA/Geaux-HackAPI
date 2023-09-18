74486973905eca8d9cee50a5a7f96fbe4dd53f95
RedPandaGA <103851430+RedPandaGA@users.noreply.github.com>
2023-09-18 13:20:54 -0500

    preped for workshop v1

--- apibackend/routes/index.js
+++ apibackend/routes/index.js
@@ -2,61 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send('<!DOCTYPE html> \
-  <html>\
-  \
-  <head>\
-      <title>Geaux Hack is Awesome!!</title>\
-  </head>\
-  \
-  <body>\
-      <div>\
-          <h1> Good morning! </h1>\
-          <p>Hello World!</p>\
-      </div>\
-      <div>\
-          <p>My name is Michelle!</p>\
-          <p>This is <strong>bold</strong></p>\
-          <p>This is <em>italics</em></p>\
-          <p> if you have a long paragraph and you dont want\
-              <br>to make another paragraph tag, you do break\
-          </p>\
-      </div>\
-      <div>\
-          <ul>\
-              <li>html</li>\
-              <li>css</li>\
-              <li>javascript</li>\
-          </ul>\
-          <ol>\
-              <li>html</li>\
-              <li>css</li>\
-              <li>javascript</li>\
-          </ol>\
-      </div>\
-      <div>\
-          <img src="apple.png" width="100" />\
-          <a href = "saselsu.org">SASE LSU\'s website!</a>\
-          <!-- a comment -->\
-      </div>\
-      <div>\
-          <form action="./example.html" method="POST">\
-              <input type="text" name="first_text_field">\
-              <input type="submit" value="Submit">\
-          </form>\
-      </div>\
-  </body>\
-  \
-  </html>')
+router.post('/translate/:target', function(req, res, next) {
+    console.log("called here")
+    if(req.headers.coolapikey == "Geaux-Hack"){
+        console.log("called here2")
+        next();
+    } else {
+        console.log("called here3")
+        res.send(JSON.stringify({error: "No valid API key!"}))
+    }
 });
 
 router.get('/test', function(req, res) {
     res.send('test');
 });
 
+router.get('/redirecttest', function(req, res) {
+    res.send('Cool Redirect');
+});
+
 router.get('/redirect', function(req, res) {
-    res.redirect('/test');
+    res.redirect('/redirecttest');
 });
 
 router.post('/submitted', function(req, res) {
@@ -65,12 +31,49 @@ router.post('/submitted', function(req, res) {
 });
 
 router.get('/submitted', function(req, res) {
-    res.send('GET Submitted text: ' + req.body.first_text_field);
+    res.send('GET Submitted text: ' + req.query.first_text_field);
 });
 
-router.post('/query', function(req, res) {
+
+
+router.post('/translate/:target', function(req, res) {
     console.log(req.body);
-    res.send('Translated Text: ' + req.body.textToTranslate);
+    
+    const options = {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({q: req.body.textToTranslate, source: "en", target: req.params.target, format: "text", api_key: ""})
+    };
+
+    console.log(options);
+    fetch("https://libretranslate.den.infamousredpanda.com/translate", options).then(apires => apires.json())
+        .then(translatedtext => {
+            if(translatedtext.error != undefined){
+                res.status(400).send(JSON.stringify(translatedtext));
+            } else {
+                resjson = { translatedText: translatedtext.translatedText };
+                console.log(resjson);
+                res.send(JSON.stringify(resjson));
+            }
+        })
+        .catch(err => {
+            console.log("error");
+        })
 });
+
+router.get('/possibletranslations', function(req, res) {
+    fetch("https://libretranslate.den.infamousredpanda.com/languages").then(apires => apires.json())
+        .then(suplang => {
+                resjson = suplang[0].targets;
+                console.log(resjson);
+                res.send(JSON.stringify(resjson));
+        })
+        .catch(err => {
+            console.log("error");
+        })
+})
 
 module.exports = router;
